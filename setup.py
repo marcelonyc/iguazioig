109d08526fbb978b7bbef10b6382ebd2152feef1
Marcelo <marcelol@iguazio.com>
2020-09-07 12:33:20 -0400

    Added dependent other files

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setuptools.setup(
     version="0.0.1",
     author="Marcelo Litovsky",
     author_email="marcelo.litovsky@gmail.com",
+    install_requires=["mlrun>=0.5.1","v3io_frames","v3io"],
+    package_data={"": ["*.ipynb"]},
     description="Wrapper functions to build an inference pipeline in Iguazio",
     url="https://github.com/marcelonyc/iguazioig",
     packages=setuptools.find_packages(),
