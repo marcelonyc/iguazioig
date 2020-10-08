324187c3abc5df52deee015ac5a292bf7f94b262
Marcelo <marcelol@iguazio.com>
2020-10-08 11:40:00 -0400

    Working on multiple trigger streams

--- iguazioig/deployment.py
+++ iguazioig/deployment.py
@@ -8,6 +8,7 @@ from iguazioig.apiv1alpha1 import create_streams_v1alpha1,_deploy_v1alpha1
 from iguazioig.apiv2alpha1 import create_streams_v2alpha1,_deploy_v2alpha1
 from iguazioig.apiv2alpha2 import create_streams_v2alpha2,_deploy_v2alpha2
 from iguazioig.apiv2alpha3 import create_streams_v2alpha3,_deploy_v2alpha3
+from iguazioig.apiv0_1 import create_streams_v0_1,_deploy_v0_1
 
 
 def deploy(yaml_file=''):
